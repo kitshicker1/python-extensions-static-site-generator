0cbf58b52c2ff5b0ce05bae02208518ee58a28b4
kitshicker1 <102931778+kitshicker1@users.noreply.github.com>
2022-08-31 09:56:48 -0500

    8/31

--- ssg/site.py
+++ ssg/site.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
-from ssg import extensions
+from ssg import extensions, hooks
 
 
 class Site:
@@ -29,12 +29,14 @@ class Site:
 
     def build(self):
         extensions.load_bundled()
+        hooks.event("collect_files", self.source, self.parsers)
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
             elif path.is_file():
                 self.run_parser(path)
+        
 
     @staticmethod
     def error(message):
